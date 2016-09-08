b938599994f188ecc1c4956330b701cc3350569d
Sebastian del Valle <sebastian.delvalle@engiefactory.com>
2016-09-08 12:11:10 -0300

    progreso

--- modulos/api.js
+++ modulos/api.js
@@ -28,11 +28,29 @@ module.exports = function (config, db) {
   });
 
   //ACCIONES
-  app.all('/:action', function (req,res) {
-    let action = db[`${req.method}_${req.params.action}`];
-    if (!action) { log(`Recibida solicitud invalida: ${req.params.action}`); return res.status(400).send('Accion o metodo invalido.'); }
-    log(`Ejecutando ${req.method} ${req.params.action}`);
-    action(res, req.body.params);
+  app.get('/config', function (req,res) {
+    db.all(`select * from config`, function (err, params) {
+      log('Respondiendo config');
+      var conf={};
+      for (let i in params) conf[params[i].param] = params[i].value;
+      res.json(conf);
+    })
+  })
+
+  app.post('/config', function (req,res) {
+    var query='';
+    var params=req.body;
+
+    //guardar parametros en la config (pisar si ya existen)
+    db.serialize(function () {
+      for (let param in params) {
+        log(`Seteando "${param}" a "${params[param]}"`);
+        db.run(`delete from config where param='${param}'`);
+        db.run(`INSERT INTO config values ('${param}', '${params[param]}')`);
+      }
+      res.json({status:'ok'});
+    })
+
   })
 
 
--- modulos/db.js
+++ modulos/db.js
@@ -18,32 +18,4 @@ db.run("CREATE TABLE IF NOT EXISTS config (param TEXT, value TEXT)", function (e
 });
 
 
-module.exports = {
-
-    /**
-     * Guarda los ajustes enviados en un JSON
-     */
-    POST_setConfig: function (res, params) {
-        var query='';
-        for (let param in params) {
-          log(`Seteando "${param}" a "${params[param]}"`);
-          query+=`delete from config where param='${param}'; INSERT INTO config values ('${param}', '${params[param]}');`
-        }
-        log('query', query)
-        db.run(query, function (err) {
-          if (err) res.json({status:'error', msg:err});
-          else res.json({status:'ok'});
-        });
-
-    },
-
-    /**
-     * Entrega la configuración
-     */
-    GET_getConfig: function (res) {
-      db.all(`select * from config`, function (err, params) {
-        log('Respondiendo config');
-        res.json(params)
-      })
-    }
-}
+module.exports = db;

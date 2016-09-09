b0e29d281f41cb51a86f89303c762b757f84ae57
Sebastian del Valle <sebastian.delvalle@engiefactory.com>
2016-09-09 11:25:36 -0300

    progreso

--- modulos/cron.js
+++ modulos/cron.js
@@ -6,7 +6,8 @@
  const log = require('../util/log')('CRON');
  const cron = require('node-cron');
  const config = require('../config');
- 
+ const email = require('../email');
+
  log(`OK cada ${config.cronCadence} minutos`);
 
  cron.schedule('*/'+config.cronCadence+' * * * *', function () {

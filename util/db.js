32d3bf723a85b1d3c8e0fe97c49dae82687065f0
Sebastian del Valle <sebastian.delvalle@engiefactory.com>
2016-09-13 12:20:46 -0300

    progreso

--- util/db.js
+++ util/db.js
@@ -6,7 +6,7 @@
 'use strict';
 const log = require('../util/log')('DB   ');
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('datos/db.sqlite');
+const db = new sqlite3.Database('db.sqlite');
 
 db.run("CREATE TABLE IF NOT EXISTS config (param TEXT, value TEXT)", function (err) {
   if(err) return log(`${'ERROR'.red.bold} ${err}`);

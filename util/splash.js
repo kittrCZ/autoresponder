a24c654769658dfbb618feccafa709fab9abe60b
Sebastian del Valle <sebastian.delvalle@engiefactory.com>
2016-09-14 09:23:03 -0300

    progreso

--- util/splash.js
+++ util/splash.js
@@ -1,17 +1,17 @@
 require('colors');
+
 var splash ='\n';
 splash+='                                   \n'.bgBlue;
 splash+='       Climo Auto Responder        \n'.bgBlue.white;
 splash+='                                   \n'.bgBlue;
 console.log(splash);
 
-
-var sigint = '\n';
-sigint+='                \n'.bgRed;
-sigint+='    Adiós :)    \n'.bgRed.white;
-sigint+='                \n'.bgRed;
-
 process.on('SIGINT',()=>{
-  console.log(sigint);
+  console.log('\n    Adiós :)    \n'.bgRed.white);
+  process.exit();
+})
+
+process.on('SIGUSR2',()=>{
+  console.log('  Reiniciando via Nodemon  \n'.bgRed.white);
   process.exit();
 })
